2a65a98efc74ec2185d9cacc6b6e087e022348e9
JunichiroKohari <junichiro@kohari.jp>
2021-07-12 21:33:35 +0900

    create users table migration

---
+++ backend/database/migrations/2021_07_12_121643_create_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('username')->unique()->comment('ユーザ名');
+            $table->string('name')->nullable()->comment('名前');
+            $table->string('email')->nullable()->comment('メールアドレス');
+            $table->foreignId('area_id')->constrained()->nullable();
+            $table->string('remark')->nullable()->comment('備考');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
